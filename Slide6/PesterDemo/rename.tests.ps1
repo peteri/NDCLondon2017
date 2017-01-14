9a24bc0a4ed152d90bfa677381103ab0e1a88a2f
Peter Ibbotson <peter@ibbotson.co.uk>
2017-01-14 18:01:46 +0000

    Before we add the location

---
+++ Slide6/PesterDemo/rename.tests.ps1
@@ -0,0 +1,10 @@
+﻿Describe "Rename" {
+	Context "Rename Item is called" {
+		$filename= 'c:\temp\test.txt'
+		Mock Rename-Item  {}
+		It "Runs" {
+			.\Rename.ps1 -Path $filename
+			Assert-MockCalled Rename-Item -ParameterFilter {$path -eq $filename -and $newName -eq "c:\temp\test.txt.bak"}
+		}
+	}
+}
