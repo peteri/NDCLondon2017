fb918bc42ffcc0b1250219c41bcc8cce886a5135
Peter Ibbotson <peter@ibbotson.co.uk>
2017-01-15 22:18:11 +0000

    Added binary version

---
+++ CocktailFinder/CocktailModule/GetIngredients.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailModule
+{
+    [Cmdlet(VerbsCommon.Get,"Ingredient")]
+    public class GetIngredients : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var ingredients=Cocktail.IngredientsReader.Read();
+            WriteObject(ingredients, true);
+        }
+    }
+}
---
+++ CocktailFinder/CocktailModule/GetRecipe.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Management.Automation;
+using Cocktail;
+
+namespace CocktailModule
+{
+    [Cmdlet(VerbsCommon.Get, "Recipe")]
+    public class GetRecipe : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "Ingredients",
+            ValueFromPipeline = true)]
+        public Ingredient[] Ingredients { get; set; }
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "Recipes",
+            ValueFromPipeline = true)]
+        public string[] Names { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var ingredients = IngredientsReader.Read();
+            var recipes = RecipesReader.Read(ingredients);
+            if (Names != null)
+            {
+                foreach (var name in Names)
+                {
+                    WriteObject(recipes[name]);
+                }
+            }
+            else
+            {
+                var inStock = Ingredients.ToList();
+                var finder = new RecipeFinder(recipes);
+                var canMake = finder.GetByAvailableIngredients(inStock);
+                foreach (var name in canMake)
+                {
+                    WriteObject(recipes[name]);
+                }
+            }
+        }
+    }
+}
