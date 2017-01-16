db63689b417ea3650dfcdbcd9b535409ab5895b3
Peter Ibbotson <peter@ibbotson.co.uk>
2017-01-16 20:28:33 +0000

    Added stuff for slide13

--- Slide13/AlcoholicCocktails.ps1
+++ Slide13/AlcoholicCocktails.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module .\CocktailModule -Force
+﻿Import-Module ..\CocktailFinder\CocktailModule\bin\debug\CocktailModule -Force
 $ingredients=Get-Ingredient
 $alcoholic = $ingredients.Values |  Where-Object {$_.AlcoholByVolume -gt 30}
 ,($alcoholic) | get-recipe
